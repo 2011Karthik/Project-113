d7511fa22b8ffbfb6c705b019ffef04674212a46
2011Karthik <100004622+2011Karthik@users.noreply.github.com>
2023-02-27 16:53:02 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+
+    tint_color = "";
+}
+
+function draw() {
+    image(video, 40, 40, 560, 400);
+    fill("red");
+    circle(40, 40, 60);
+    circle(600, 40, 60);
+    circle(40, 440, 60);
+    circle(600, 440, 60);
+    fill("green");
+    rect(60, 35, 520, 20)
+    rect(60, 430, 520, 20)
+    rect(40, 35, 20, 415)
+    rect(580, 35, 20, 415)
+}
+
+function take_snapshot(){
+     save('student_name.png');
+}
